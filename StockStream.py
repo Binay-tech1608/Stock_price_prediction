83acaf90c076d9860288ed3d654a5f90d3907258
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2024-01-30 12:04:39 +0530

    Update StockStream.py

--- StockStream.py
+++ StockStream.py
@@ -283,5 +283,3 @@ elif(selected == 'About'):
     
     st.markdown('<p class="big-font">StockStream is a web application that allows users to visualize Stock Performance Comparison, Real-Time Stock Prices and Stock Price Prediction. This application is developed using Streamlit. Streamlit is an open source app framework in Python language. It helps users to create web apps for Data Science and Machine Learning in a short time. This Project is developed by BINAY MANN,ABIR BISWAS and BHUBAN RUIDAS. You can find more about the developers on their GitHub Profiles shared below.<br>Hope you are able to employ this application well and get your desired output.<br> Cheers!</p>', unsafe_allow_html=True)
     st.subheader('BINAY MANN [![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/Binay-tech1608) ')
-    st.subheader('ABIR BISWAS [![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/ALphastrEAM5) ')
-    st.subheader('BHUBAN RUIDAS [![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/ALphastrEAM5) ')
